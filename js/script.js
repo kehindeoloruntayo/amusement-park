1a5b5d73ed61311674ade3d5002f04bba744e47a
kehindeoloruntayo <kehindeoloruntayo1@gmail.com>
2023-05-04 23:48:47 +0100

    add javascript folder

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $('#height-form').submit(function (event) {
+    event.preventDefault();
+    const height = $('#height-input').val();
+    if (height < 89) {
+      window.location.href = "short.html";
+    } else if (height >= 122) {
+      window.location.href = "tootall.html";
+    } else {
+      window.location.href = "tall.html";
+    }
+  })
+
+  $('#tooTall-btn').click(function() {
+    $('.colour').removeClass("addcolour");
+    $('.colour').addClass("addcolour");
+    $('.none').show();
+  });
+  $('#short-btn').click(function() {
+    $('.colour').removeClass("addcolour");
+    $('.colour').addClass("addcolour");
+    $('.none').show();
+  });
+});
